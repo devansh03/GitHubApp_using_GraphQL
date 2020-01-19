a303985d084820d5dcf3d33b213af59f8f4611c6
Devansh Baldwa <devanshbaldwa@gmail.com>
2020-01-19 12:37:09 +0530

    Everything done, only variable should be added to the query

--- githubapp/lib/main.dart
+++ githubapp/lib/main.dart
@@ -1,60 +1,9 @@
-//import 'package:flutter/material.dart';
-//import 'package:graphql_flutter/graphql_flutter.dart';
-//import 'package:http/http.dart';
-//
-//
-//void main() => runApp(myApp());
-//
-//class myApp extends StatefulWidget {
-//  @override
-//  _myAppState createState() => _myAppState();
-//}
-//
-//class _myAppState extends State<myApp> {
-//
-//
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: "GitHub Profiles",
-//      debugShowCheckedModeBanner: false,
-//      home: Home(),
-//    );
-//  }
-//}
-//
-//
-//class Home extends StatefulWidget {
-//  @override
-//  _HomeState createState() => _HomeState();
-//}
-//
-//class _HomeState extends State<Home> {
-//
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("GITHUB PROFILE"),
-//        backgroundColor: Colors.grey[900],
-//      ),
-//
-//    );
-//  }
-//}
-//
-//
-
-// GITHUB Personal Access Token -  a023161f53615b2e5360e63511b3002a7fc28dfa
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() => runApp(MaterialApp(
-      title: "GitHub",
+      title: "GitHub_using_GraphQL",
       debugShowCheckedModeBanner: false,
       home: MyApp(),
     ));
@@ -86,318 +35,32 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-//class MyHomePage extends StatefulWidget {
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  @override
-//  Widget build(BuildContext context) {
-//    String readRepositories = """
-//
-//        query GitHubApp_Devansh{
-//            user(login: "wizpna"){
-//              avatarUrl(size: 200)
-//              location
-//              name
-//              url
-//              email
-//              login
-//              repositories{
-//                totalCount
-//              }
-//              following{
-//                totalCount
-//              }
-//            }
-//            viewer {
-//              starredRepositories(last: 12){
-//                edges{
-//                  node{
-//                    id
-//                    name
-//                    nameWithOwner
-//                  }
-//                }
-//              }
-//            }
-//        }
-//    """;
-//
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text("GitHub"),
-//        backgroundColor: Colors.black,
-//        centerTitle: true,
-//      ),
-//      body: Query(
-//          options: QueryOptions(document: readRepositories),
-//          builder: (QueryResult result,
-//              {VoidCallback refetch, FetchMore fetchMore}) {
-//            if (result.hasException == null) {
-//              return Center(
-//                child: Text(
-//                  result.hasException.toString(),
-//                  style: TextStyle(fontSize: 18),
-//                  textAlign: TextAlign.center,
-//                ),
-//              );
-//            }
-//            if (result.loading) {
-//              return Center(
-//                child: CircularProgressIndicator(),
-//              );
-//            }
-//            final userDetails = result.data['user'];
-//            List starredRepositories =
-//                result.data['viewer']['starredRepositories']['edges'];
-//
-//            return Stack(
-//              children: <Widget>[
-//                Column(
-//                  crossAxisAlignment: CrossAxisAlignment.start,
-//                  children: <Widget>[
-//                    Container(
-//                      color: Colors.black,
-//                      alignment: Alignment.center,
-//                      child: Column(
-//                        children: <Widget>[
-//                          SizedBox(
-//                            height: 2,
-//                          ),
-//                          ClipOval(
-//                            child: Image.network(
-//                              userDetails["avatarUrl"],
-//                              filterQuality: FilterQuality.high,
-//                              fit: BoxFit.cover,
-//                              height: 100.0,
-//                              width: 100.0,
-//                            ),
-//                          ),
-//                          SizedBox(
-//                            height: 5,
-//                          ),
-//                          Text(
-//                            userDetails['name'],
-//                            style: TextStyle(
-//                                fontSize: 18,
-//                                fontWeight: FontWeight.w800,
-//                                color: Colors.white),
-//                          ),
-//                          SizedBox(
-//                            height: 5,
-//                          ),
-//                          Text(
-//                            userDetails['login'],
-//                            style: TextStyle(
-//                                fontSize: 15,
-//                                fontWeight: FontWeight.w500,
-//                                color: Colors.grey),
-//                          ),
-//                          SizedBox(
-//                            height: 10,
-//                          ),
-//                          Row(
-//                            mainAxisAlignment: MainAxisAlignment.center,
-//                            children: <Widget>[
-//                              Icon(
-//                                Icons.location_on,
-//                                color: Colors.grey,
-//                                size: 16,
-//                              ),
-//                              SizedBox(
-//                                width: 5,
-//                              ),
-//                              Text(
-//                                userDetails['location'],
-//                                style: TextStyle(
-//                                    fontSize: 15,
-//                                    fontWeight: FontWeight.w500,
-//                                    color: Colors.grey),
-//                              )
-//                            ],
-//                          ),
-//                          SizedBox(
-//                            height: 8,
-//                          ),
-//                          Row(
-//                            mainAxisAlignment: MainAxisAlignment.center,
-//                            children: <Widget>[
-//                              Icon(
-//                                Icons.link,
-//                                color: Colors.grey,
-//                                size: 16,
-//                              ),
-//                              SizedBox(
-//                                width: 5,
-//                              ),
-//                              Text(
-//                                userDetails['url'],
-//                                style: TextStyle(
-//                                    fontSize: 15,
-//                                    fontWeight: FontWeight.w500,
-//                                    color: Colors.grey),
-//                              )
-//                            ],
-//                          ),
-//                          SizedBox(
-//                            height: 8,
-//                          ),
-//                          Row(
-//                            mainAxisAlignment: MainAxisAlignment.center,
-//                            children: <Widget>[
-//                              Icon(
-//                                Icons.email,
-//                                color: Colors.grey,
-//                                size: 16,
-//                              ),
-//                              SizedBox(
-//                                width: 5,
-//                              ),
-//                              Text(
-//                                userDetails['email'],
-//                                style: TextStyle(
-//                                    fontSize: 15,
-//                                    fontWeight: FontWeight.w500,
-//                                    color: Colors.grey),
-//                              )
-//                            ],
-//                          ),
-//                          SizedBox(
-//                            height: 15,
-//                          ),
-//                          Padding(
-//                            padding:
-//                                const EdgeInsets.only(left: 10.0, right: 10),
-//                            child: Row(
-//                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                              children: <Widget>[
-//                                Column(
-//                                  children: <Widget>[
-//                                    Text(
-//                                      userDetails["repositories"]['totalCount']
-//                                          .toString(),
-//                                      style: TextStyle(
-//                                          color: Colors.white,
-//                                          fontSize: 18,
-//                                          fontWeight: FontWeight.bold),
-//                                    ),
-//                                    Text(
-//                                      "Repositories",
-//                                      style: TextStyle(
-//                                          fontSize: 15,
-//                                          fontWeight: FontWeight.w500,
-//                                          color: Colors.grey),
-//                                    )
-//                                  ],
-//                                ),
-//                                Column(
-//                                  children: <Widget>[
-//                                    Text(
-//                                      userDetails["followers"]['totalCount']
-//                                          .toString(),
-//                                      style: TextStyle(
-//                                          color: Colors.white,
-//                                          fontSize: 18,
-//                                          fontWeight: FontWeight.bold),
-//                                    ),
-//                                    Text(
-//                                      "Followers",
-//                                      style: TextStyle(
-//                                          fontSize: 15,
-//                                          fontWeight: FontWeight.w500,
-//                                          color: Colors.grey),
-//                                    )
-//                                  ],
-//                                ),
-//                                Column(
-//                                  children: <Widget>[
-//                                    Text(
-//                                      userDetails["following"]['totalCount']
-//                                          .toString(),
-//                                      style: TextStyle(
-//                                          color: Colors.white,
-//                                          fontSize: 18,
-//                                          fontWeight: FontWeight.bold),
-//                                    ),
-//                                    Text(
-//                                      "Following",
-//                                      style: TextStyle(
-//                                          fontSize: 15,
-//                                          fontWeight: FontWeight.w500,
-//                                          color: Colors.grey),
-//                                    )
-//                                  ],
-//                                )
-//                              ],
-//                            ),
-//                          ),
-//                          SizedBox(
-//                            height: 8,
-//                          ),
-//                        ],
-//                      ),
-//                    ),
-//                    Padding(
-//                      padding:
-//                          const EdgeInsets.only(top: 10.0, bottom: 2, left: 10),
-//                      child: Text(
-//                        "Starred Repositories",
-//                        style: TextStyle(
-//                            fontSize: 20, fontWeight: FontWeight.bold),
-//                      ),
-//                    ),
-//                    Divider(),
-//                  ],
-//                ),
-//                Padding(
-//                  padding: const EdgeInsets.only(top: 330.0),
-//                  child: ListView.builder(
-//                    itemCount: starredRepositories.length,
-//                    itemBuilder: (context, index) {
-//                      final repository = starredRepositories[index];
-//                      return Padding(
-//                        padding: const EdgeInsets.only(
-//                            left: 10.0, top: 8, bottom: 8),
-//                        child: Card(
-//                          elevation: 0,
-//                          child: Row(
-//                            children: <Widget>[
-//                              Icon(Icons.collections_bookmark),
-//                              SizedBox(
-//                                width: 5,
-//                              ),
-//                              Text(
-//                                repository['node']['nameWithOwner'],
-//                                style: TextStyle(
-//                                    fontSize: 16, fontWeight: FontWeight.w500),
-//                              ),
-//                            ],
-//                          ),
-//                        ),
-//                      );
-//                    },
-//                  ),
-//                )
-//              ],
-//            );
-//          }),
-//    );
-//  }
-//}
-
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final TextEditingController _nameController = new TextEditingController();
+  String name = "devansh03";
+  String readRepositories;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
-    String readRepositories = """
+    if(_nameController.toString().isNotEmpty){
+      name = _nameController.toString();
+    }
+
+    readRepositories = """
     query Flutter_Github_GraphQL{
-            user(login: "wizpna") {
+            user(login: "devansh03") {
                 avatarUrl(size: 200)
                 location
                 name
@@ -426,18 +89,57 @@ class _MyHomePageState extends State<MyHomePage> {
               }
             }
           }
-      """;
+      """ ;
+
 
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text("GitHub"),
+          title: Text("GitHub",style: TextStyle(fontSize: 21.0, fontWeight: FontWeight.w500),),
+
           backgroundColor: Colors.black,
-          centerTitle: true,
+          centerTitle: false,
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 150.0,
+                color: Colors.tealAccent.shade100,
+                child: Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      labelText: "Enter UserName",
+                      border: OutlineInputBorder(
+                        gapPadding: 3.3,
+                        borderRadius: BorderRadius.circular(3.3)
+                      )
+                    ),
+                    controller: _nameController,
+                  ),
+                ),
+              ),
+            ),
+            IconButton(icon: Icon(Icons.search), onPressed: (){
+              setState(() {
+                  name = _nameController.toString();
+                  MyHomePage();
+              });
+//              Navigator.pushReplacement(
+//                  context,
+//                  MaterialPageRoute(builder: (_) => MyHomePage()))
+//                  .then((_) => MyHomePage());
+            })
+          ],
         ),
         body: Query(
           options: QueryOptions(
+            // ignore: deprecated_member_use
             document: readRepositories,
+            variables: {
+              'name': "devansh03",
+            },
           ),
           builder: (QueryResult result,
               {VoidCallback refetch, FetchMore fetchMore}) {
@@ -449,6 +151,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     textAlign: TextAlign.center,
                   ));
             }
+
+//            if (result.noSuchMethod() != null){
+//              return Text(result.hasException.toString());
+//            }
 
             if (result.loading) {
               return Center(child: CircularProgressIndicator());
@@ -648,7 +354,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       padding:
                       const EdgeInsets.only(top: 10.0, bottom: 2, left: 10),
                       child: Text(
-                        "Starred Repositories",
+                        "devansh03's Starred Repositories",
                         style:
                         TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       ),
@@ -678,7 +384,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                   repository['node']['nameWithOwner'],
                                   overflow: TextOverflow.ellipsis,
                                   style: TextStyle(
-                                      fontSize: 16, fontWeight: FontWeight.w500),
+                                      fontSize: 14, fontWeight: FontWeight.w500),
                                 ),
                               ],
                             ),
